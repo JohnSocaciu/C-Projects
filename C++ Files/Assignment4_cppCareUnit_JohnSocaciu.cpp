2621b9d654cbe8966c9e5bc9b5a81c4831c26d65
JohnSocaciu <94732414+JohnSocaciu@users.noreply.github.com>
2022-09-17 18:38:59 -0400

    Add files via upload

---
+++ C++ Files/Assignment4_cppCareUnit_JohnSocaciu.cpp
@@ -0,0 +1,57 @@
+/*==========================================================
+
+ Title:     Assignment 4 - C++ Care Unit
+ Course:  	CIS 2252
+ Author:  	John Socaciu
+ Date:    	09/17/2022
+ Description: Calculate the BMI using two inputs. 
+
+ ==========================================================
+*/
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+
+// Write functions here
+
+double BMIcalculator(double weight){
+    double bmiKilograms = weight / 3.24; 
+    return bmiKilograms; 
+}
+
+double conversionMeters(double height){
+    double heightMeters = height / 3.281;
+    return heightMeters; 
+}
+
+double BMIcalculator(double weightKilograms, double heightMeters){
+    double newBmi = weightKilograms / (heightMeters * heightMeters); 
+    return newBmi; 
+}
+
+void swap(double& firstWeight, double& secondWeight){
+    double firstStorage = firstWeight; 
+    double secondStorage = secondWeight;
+    firstWeight = secondStorage;
+    secondWeight = firstStorage; 
+}
+
+int main() {
+    //DO NOT MODIFY MAIN()!!!
+    
+    double w1, w2, h;
+    cin >> w1 >> w2 >> h;
+
+    float index1 = BMIcalculator(w1);
+    float index2 = BMIcalculator(w2, conversionMeters(h));
+
+    cout << index1 << " " << index2 << endl;
+
+    swap(w1, w2);
+
+    index1 = BMIcalculator(w1);
+    index2 = BMIcalculator(w2, conversionMeters(h));
+    cout << index1 << " " << index2 << endl;
+}
